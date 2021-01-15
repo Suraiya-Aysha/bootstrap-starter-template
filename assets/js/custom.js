8d7a74fdebef603532c3ac333a6b7b2130561a2c
suraiya-aysha <sm.asa15nov@gmail.com>
2021-01-15 12:54:49 +0600

    starter template

---
+++ assets/js/custom.js
@@ -0,0 +1,75 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+// Preloader Start
+    $(window).load(function () {
+        $('#status').fadeOut();
+        $('#preloader')
+            .delay(350)
+            .fadeOut('slow');
+        $('body')
+            .delay(350)
+    });
+// Preloader End
+
+/// MAIN MENU SCRIPT START
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Dropdown Menu Script Start
+
+  $('#mainNav ul.navbar-nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+  });
+  // Dropdown Menu Script End
+
+  // mobile Menu area
+  $('nav.mobile_menu').meanmenu({
+    meanScreenWidth: '991'
+  });
+  $('nav.mean-nav li > a:first-child').on('click', function () {
+    $('a.meanmenu-reveal').click();
+  });
+  // mobile Menu area
+
+  // MAIN MENU SCRIPT END
+
+})(jQuery); // End of use strict
